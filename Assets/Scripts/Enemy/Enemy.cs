9406de46359877719d00fd7deeed3dba4eb01253
FarrellG <farrellgunawan99@gmail.com>
2025-09-06 23:28:22 +0700

    Setting up Navmesh and Interface States
    feat:
    - Adding BaseState interface with other States
    - Setting up Navmesh in Scene

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private BaseState _currentState;
+    public PatrolState PatrolState = new PatrolState();
+    public ChaseState ChaseState = new ChaseState();
+    public RetreatState RetreatState = new RetreatState();
+
+    private void Awake()
+    {
+        _currentState = PatrolState;
+        _currentState.EnterState(this);
+    }
+
+    private void Update()
+    {
+        if(_currentState != null) _currentState.UpdateState(this);
+    }
+}
