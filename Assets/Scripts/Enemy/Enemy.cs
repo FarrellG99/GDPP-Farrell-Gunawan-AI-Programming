378b40b2c46e0ae597920cc546cc4c8693ae4501
FarrellG <farrellgunawan99@gmail.com>
2025-09-06 23:48:07 +0700

    Wrap up Patrol Enemy State
    feat:
    - Finishing making enemy patrol state

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,15 +1,24 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Serialization;
 
 public class Enemy : MonoBehaviour
 {
-    private BaseState _currentState;
+    public List<GameObject> waypoints;
+
     public PatrolState PatrolState = new PatrolState();
     public ChaseState ChaseState = new ChaseState();
     public RetreatState RetreatState = new RetreatState();
 
+    [HideInInspector] public NavMeshAgent agent;
+
+    private BaseState _currentState;
+
     private void Awake()
     {
+        agent = GetComponent<NavMeshAgent>();
         _currentState = PatrolState;
         _currentState.EnterState(this);
     }
--- Assets/Scripts/Enemy/States/PatrolState.cs
+++ Assets/Scripts/Enemy/States/PatrolState.cs
@@ -2,14 +2,32 @@ using UnityEngine;
 
 public class PatrolState : BaseState
 {
+    private bool _isMoving;
+    private Vector3 _destination;
+
     public void EnterState(Enemy enemy)
     {
         Debug.Log("Start Patrol");
+        _isMoving = false;
     }
 
     public void UpdateState(Enemy enemy)
     {
         Debug.Log("Patrolling...");
+        if (!_isMoving)
+        {
+            _isMoving = true;
+            int index = Random.Range(0, enemy.waypoints.Count);
+            _destination = enemy.waypoints[index].transform.position;
+            enemy.agent.destination = _destination;
+        }
+        else
+        {
+            if (Vector3.Distance(_destination, enemy.transform.position) <= 0.1f)
+            {
+                _isMoving = false;
+            }
+        }
     }
 
     public void ExitState(Enemy enemy)
